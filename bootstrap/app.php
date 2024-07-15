c9e44dd35045aec1010bdc4ecb075d7a60650554
Ben Stapleton <webdev.ben@gmail.com>
2024-07-15 09:50:19 +0100

    Fixes incorrect middleware registration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(ReturnJsonResponseMiddleware::class);
-        $middleware->append(CheckAuthed::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,21 @@ use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\FlashcardController;
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\TagController;
+use App\Http\Middleware\CheckAuthed;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', 'App\Http\Controllers\UserController@register')->name('auth.register');
 Route::post('/login', 'App\Http\Controllers\LoginController@login')->name('auth.login');
 Route::get('/user', 'App\Http\Controllers\UserController@getUserDetails')->middleware('auth:sanctum')->name('auth.user');
 
-Route::controller(AnswerController::class)->prefix('answers')->middleware('auth:sanctum')->group(function () {
+Route::controller(AnswerController::class)->prefix('answers')->middleware(['auth:sanctum', CheckAuthed::class])->group(function () {
     Route::post('/', 'store')->name('answers.store');
     Route::get('/{answer}', 'show')->name('answers.show');
     Route::patch('/{answer}', 'update')->name('answers.update');
     Route::delete('/{answer}', 'destroy')->name('answers.destroy');
 });
 
-Route::controller(FlashcardController::class)->prefix('flashcards')->middleware('auth:sanctum')->group(function () {
+Route::controller(FlashcardController::class)->prefix('flashcards')->middleware(['auth:sanctum', CheckAuthed::class])->group(function () {
     Route::get('/', 'index')->name('flashcard.index');
     Route::post('/', 'store')->name('flashcards.store');
     Route::get('/random', 'random')->name('flashcards.random');
@@ -28,13 +29,13 @@ Route::controller(FlashcardController::class)->prefix('flashcards')->middleware(
     Route::delete('/{flashcard}/tags/{tag}', 'detachTag')->name('flashcards.tags.detach');
 });
 
-Route::controller(LessonController::class)->prefix('lessons')->middleware('auth:sanctum')->group(function () {
+Route::controller(LessonController::class)->prefix('lessons')->middleware(['auth:sanctum', CheckAuthed::class])->group(function () {
     Route::get('/', 'index')->name('lessons.index');
     Route::post('/', 'store')->name('lessons.store');
     Route::get('/{lesson}', 'show')->name('lessons.show');
 });
 
-Route::controller(TagController::class)->prefix('tags')->middleware('auth:sanctum')->group(function () {
+Route::controller(TagController::class)->prefix('tags')->middleware(['auth:sanctum', CheckAuthed::class])->group(function () {
     Route::get('/', 'index')->name('tags.index');
     Route::post('/', 'store')->name('tags.store');
     Route::put('/{tag}', 'update')->name('tags.update');
