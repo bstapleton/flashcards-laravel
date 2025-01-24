0ccb364c8ff62a83a4ef6f85b6f70741de5a946c
Ben Stapleton <webdev.ben@gmail.com>
2025-01-24 11:20:16 +0000

    Adds command to reset demo user data

---
+++ app/Console/Commands/ResetDemo.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\Difficulty;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class ResetDemo extends Command
+{
+    protected $signature = 'app:reset-demo';
+
+    protected $description = 'Resets the demo user attempts and scoring, as well as password etc.';
+
+    /**
+     * @throws \Throwable
+     */
+    public function handle()
+    {
+        $user = User::find(1);
+
+        if (!$user) {
+            $this->fail('User does not exist.');
+        }
+
+        // Reset user data
+        $user->points = 0;
+        $user->email = 'f2@test.com';
+        $user->password = bcrypt('password');
+        $user->save();
+
+        // Purge flashcard attempts and reset difficulty
+        $user->flashcards->map(function ($flashcard) {
+            $flashcard->difficulty = Difficulty::EASY;
+            $flashcard->attempts()->delete();
+            $flashcard->save();
+        });
+
+        $this->info('Demo user has been reset.');
+    }
+}
