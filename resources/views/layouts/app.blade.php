4c9ba1bf2fa30b3ab5a2f4676adfef4e9a626199
Ben Stapleton <webdev.ben@gmail.com>
2026-04-05 08:26:39 +0100

    Fix display issue for active section links on revise/answer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,10 +34,10 @@
                             <a href="{{ route('flashcards.index') }}" class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->routeIs('flashcards.*') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium">
                                 My Flashcards
                             </a>
-                            <a href="{{ route('revision.index') }}" class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->routeIs('revision') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium">
+                            <a href="{{ route('revision.index') }}" class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->routeIs('revision.*') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium">
                                 Revise
                             </a>
-                            <a href="{{ route('answer.index') }}" class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->routeIs('answer') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium">
+                            <a href="{{ route('answer.index') }}" class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->routeIs('answer.*') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium">
                                 Answer
                             </a>
                         </div>
