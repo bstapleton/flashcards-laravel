f73432f13145a051af2137bb2a3bf693cf22d0bb
Ben Stapleton <webdev.ben@gmail.com>
2026-05-11 09:42:24 +0100

    Fix incorrect application of naming change in migration

--- database/migrations/2024_04_13_070531_create_tags_table.php
+++ database/migrations/2024_04_13_070531_create_tags_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('subjects', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->text('name');
@@ -18,6 +18,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('subjects');
+        Schema::dropIfExists('tags');
     }
 };
